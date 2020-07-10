d53b411be21d605d72ed4a7c99bc2c96f4edef18
vibhuk10 <vibhuk10@gmail.com>
2020-07-09 18:12:50 -0700

    uploaded graphs

--- 02-peryear.R
+++ 02-peryear.R
@@ -1,4 +1,5 @@
 twopoint %>% count(year) 
 twopoint %>% count(year, two_point_conv_result)
 ggplot(data = twopoint) + 
-  geom_bar(mapping = aes(x = year, fill = two_point_conv_result))
+  geom_bar(mapping = aes(x = year, fill = two_point_conv_result)) +
+  labs(title = "Amount of two point conversions attempted per year between 2009-2018 and success rate")
--- 04-team.R
+++ 04-team.R
@@ -3,5 +3,6 @@ teams <- arrange(teams, desc(n))
 options(tibble.print_max = Inf)
 teams
 ggplot(data = twopoint) + 
-  geom_bar(mapping = aes(x = posteam, fill = two_point_conv_result))
+  geom_bar(mapping = aes(x = posteam, fill = two_point_conv_result)) +
+  labs(x = "team",title = "Amount of two point conversions attempted by each team between 2009-2018 and success rate")
 
