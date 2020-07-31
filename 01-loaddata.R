3c97c6b3da2b02cb919028c91b9b1ad1ae7ca3db
vibhuk10 <vibhuk10@gmail.com>
2020-07-30 20:59:20 -0700

    update

--- 01-loaddata.R
+++ 01-loaddata.R
@@ -58,6 +58,7 @@ twopoint <- read_csv("data-clean/twopoint_2009-2019.csv")
 extrapoint <- read_csv("data-clean/extrapoint_2009-2019.csv")
 
 kickoffs <- read_csv("data-clean/kickoffs_2009-2019.csv")
+last_plays <- read_csv("data-clean/last_plays_2009-2019.csv")
 
 twopoint2 <- read_csv("data-clean/twopoint_2009-2018.csv")
 data_09_18 <- read_csv("data-raw/NFL Play by Play 2009-2018 (v5).csv")
